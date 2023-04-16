d25651a2df003fa5bb5c83dd798e81377ac9f818
GADLO <geekarea@outlook.com>
2023-04-17 04:52:04 +0800

    ✨ feat:首页样式调整

--- client/src/components/content.js
+++ client/src/components/content.js
@@ -1,7 +1,15 @@
 export default function main() {
   return (`
       <div class='main'>
-      <div class="main-left">content</div>
+      <div class="main-left">
+          <div class="article">
+          <div class="at">title</div>
+          <div class="am">
+              <div class="amt">content</div>
+              <div class="ami">image</div>
+          </div>
+          </div>
+      </div>
       <div class="main-right">assets</div>
       </div>
     `)
