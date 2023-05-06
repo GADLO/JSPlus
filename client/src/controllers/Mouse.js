3fa67d23259aa1814b66a9f6b374e2fc3cff584b
GADLO <geekarea@outlook.com>
2023-05-06 09:29:22 +0800

    ✨ feat:完善拖拽模块，新增拖拽原型方法

--- client/src/controllers/Mouse.js
+++ client/src/controllers/Mouse.js
@@ -1,5 +1,5 @@
 import Mouse from '../views/Mouse'
-import { getScrollOffset } from '../utils/event';
+import { getViewportSize, pagePos, addEvent, cancelBubble, preventDefaultEvent, removeEvent } from '../utils/event';
 import { getStyles } from '../utils/styleMethods';
 
 export async function MouseView() {
@@ -37,7 +37,6 @@ export async function mouseScript() {
 
     //mousedown mouseup mousemove
 
-
     box.onmouseup = function (e) {
         document.onmousemove = null
     }
@@ -67,23 +66,189 @@ export async function mouseScript() {
             //onmouemove的时候box位置需要减去鼠标距离box边界值，保证鼠标位于box内部
             box.style.left = page.X - x + 'px';
             box.style.top = page.Y - y + 'px'
+
+
         }
     }
 
 
 
-    function pagePos(e) {
-        //获取滚动条位置
-        var sLeft = getScrollOffset().left,
-            sTop = getScrollOffset().top,
+    //封装拖拽函数模块
+    var oLink = document.getElementsByClassName('link')[0]
+    var dragNClick = (function (elem, elemClick) {
+        //记录mouse事件时间戳，初始位置，当前窗口宽度,元素宽高
+        var beginTime = 0,
+            endTime = 0,
+            initPos = [],
+            wWidth = getViewportSize().width,
+            wHeight = getViewportSize().height,
+            elemWidth = getStyles(elem, 'width'),
+            elemHeight = getStyles(elem, 'height');
 
-            //获取文档偏移距离，有可能不存在
-            cLeft = document.documentElement.clientLeft || 0,
-            cTop = document.documentElement.clientTop || 0;
+        drag()
+        function drag() {
+            var x, y;
 
-        return {
-            X: e.clientX + sLeft + cLeft,
-            Y: e.clientY + sTop + cTop
+            addEvent(elem, 'mousedown', function (e) {
+                var e = e || window.event;
+
+                //开始时间戳
+                beginTime = new Date().getTime();
+
+                //初始位置赋值
+                initPos = [getStyles(elem, 'left'), getStyles(elem, 'top')]
+
+                //获取鼠标在相对于elem边界坐标值
+                x = pagePos(e).X - getStyles(elem, 'left');
+                y = pagePos(e).Y - getStyles(elem, 'top');
+
+                //elem跟随鼠标坐标值
+                addEvent(document, 'mousemove', mouseMove);
+                addEvent(document, 'mouseup', mouseUp);
+
+                cancelBubble(e)
+                preventDefaultEvent(e)
+            });
+
+            //获取鼠标在相对于elem边界坐标值函数
+            function mouseMove(e) {
+                var e = e || window.event,
+                    elemLeft = pagePos(e).X - x,
+                    elemTop = pagePos(e).Y - y;
+
+                //当元素超过横向边界时，重新赋值操作
+                if (elemLeft <= 0) {
+                    elemLeft = 0
+                } else if (elemLeft >= wWidth - elemWidth) {
+                    elemLeft = wWidth - elemWidth
+                }
+
+                //当元素超过纵向边界时，重新赋值操作
+                if (elemTop <= 0) {
+                    elemTop = 0
+                } else if (elemTop >= wHeight - elemHeight) {
+                    elemTop = wHeight - elemHeight
+                    console.log(1);
+                }
+
+
+                //让元素跟随拖拽位置
+                elem.style.left = elemLeft + 'px';
+                elem.style.top = elemTop + 'px'
+
+
+            }
+
+            //鼠标释放取消监听鼠标移动事件函数
+            function mouseUp(e) {
+                var e = e || window.event;
+                endTime = new Date().getTime();
+
+                //若mousedown和mouseup时间间隔小于200 则默认为奠基事件，反之为拖拽事件
+                if (endTime - beginTime < 200) {
+                    elemClick();
+                    elem.style.left = initPos[0];
+                    elem.style.top = initPos[1];
+                }
+
+
+                removeEvent(document, 'mouseup', mouseUp);
+                removeEvent(document, 'mousemove', mouseMove);
+
+            }
         }
-    }
+    })
+
+    // dragNClick(oLink, function () {
+    //     window.open('http://google.com')
+    // })
+
+    //封装拖拽函数进Elemnt原型
+    Element.prototype.dragNClick = (function (elemClick) {
+        //记录mouse事件时间戳，初始位置
+        var beginTime = 0,
+            endTime = 0,
+            initPos = [],
+            wWidth = getViewportSize().width,
+            wHeight = getViewportSize().height,
+            elemWidth = getStyles(this, 'width'),
+            elemHeight = getStyles(this, 'height');
+
+        //改变this指向，指向当前调用元素
+        drag.call(this)
+
+        function drag() {
+            var x, y, _self = this;
+
+            addEvent(this, 'mousedown', function (e) {
+                var e = e || window.event;
+
+                //开始时间戳
+                beginTime = new Date().getTime();
+
+                //初始位置赋值
+                initPos = [getStyles(_self, 'left'), getStyles(_self, 'top')]
+
+                //获取鼠标在相对于elem边界坐标值
+                x = pagePos(e).X - getStyles(_self, 'left');
+                y = pagePos(e).Y - getStyles(_self, 'top');
+
+                //elem跟随鼠标坐标值
+                addEvent(document, 'mousemove', mouseMove);
+                addEvent(document, 'mouseup', mouseUp);
+
+                cancelBubble(e);
+                preventDefaultEvent(e);
+            });
+
+            //获取鼠标在相对于elem边界坐标值函数
+            function mouseMove(e) {
+                var e = e || window.event,
+                    elemLeft = pagePos(e).X - x,
+                    elemTop = pagePos(e).Y - y;
+
+                //当元素超过横向边界时，重新赋值操作
+                if (elemLeft <= 0) {
+                    elemLeft = 0
+                } else if (elemLeft >= wWidth - elemWidth) {
+                    elemLeft = wWidth - elemWidth
+                }
+
+                //当元素超过纵向边界时，重新赋值操作
+                if (elemTop <= 0) {
+                    elemTop = 0
+                } else if (elemTop >= wHeight - elemHeight) {
+                    elemTop = wHeight - elemHeight
+
+                }
+
+                //让元素跟随拖拽位置
+                _self.style.left = elemLeft + 'px';
+                _self.style.top = elemTop + 'px'
+
+            }
+
+            //鼠标释放取消监听鼠标移动事件函数
+            function mouseUp(e) {
+                var e = e || window.event;
+                endTime = new Date().getTime();
+
+                //若mousedown和mouseup时间间隔小于200 则默认为奠基事件，反之为拖拽事件
+                if (endTime - beginTime < 200) {
+                    elemClick();
+                    _self.style.left = initPos[0];
+                    _self.style.top = initPos[1];
+                }
+
+                removeEvent(document, 'mouseup', mouseUp);
+                removeEvent(document, 'mousemove', mouseMove);
+
+            }
+        }
+    })
+
+
+    oLink.dragNClick(function () {
+        window.open('http://google.com')
+    })
 }
