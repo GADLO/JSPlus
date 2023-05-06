7f5fc6b488ad7889320b0f0740b3f3879977a386
GADLO <geekarea@outlook.com>
2023-05-06 09:28:48 +0800

    ✨ feat:完善拖拽模块，新增拖拽原型方法

--- client/src/views/Mouse.js
+++ client/src/views/Mouse.js
@@ -3,101 +3,24 @@ import mouse from '../styles/mouse.css'
 
 export default function Mouse() {
     return (`
-    <div class="box"></div>
-    <br>
-    <br>
-
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <br>
-    <br><br><br><br>
-
-
-    <br><br><br><br>
-    <br>
-    <br>
-
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <br>
-    <br><br><br><br>
-
-
-    <br><br><br><br>
-    <br>
-    <br>
-
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <br>
-    <br><br><br><br>
-
-
-    <br><br><br><br>
-    <br>
-    <br>
-
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <br>
-    <br><br><br><br>
-
-
-    <br><br><br><br>
-    <br>
-    <br>
-
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <br>
-    <br><br><br><br>
-
-
-    <br><br><br><br>
+    <a href="javascript:;" class="link">link</a>
+    <div class='box'><div>
     `)
 }
+
+//href="javascript:;"这个是为了防止a标签的href生效
+
+
+//等待执行模块化编写写法
+var moduleA = (function (a, b) {
+    console.log(a, b);
+})
+
+moduleA(1, 2);
+
+
+//立即执行模块化写法
+(function (a, b) {
+    console.log(a, b);
+})(3, 4);
+
